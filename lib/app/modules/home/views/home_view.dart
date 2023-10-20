0b3613d2296f56b35eab82db46d20dbf8282bcc0
WarmH <WarmHobo95@outlook.com>
2023-10-21 01:22:57 +0500

    routes_done

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:clitest03/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,16 +10,9 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: ElevatedButton(onPressed:() => Get.toNamed(Routes.LOGIN), child: const Text("login"),),
         ),
-      ),
-    );
+      );
   }
 }
